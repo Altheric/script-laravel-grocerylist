5e61d41709f00a461ff2b9cd2b99f536a7e88ec3
Altheric <mauricedx@gmail.com>
2024-08-23 12:09:26 +0200

    Fixed database viewing

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Item;
+use Illuminate\Http\Request;
+
 
 class ItemController extends Controller
 {
@@ -13,7 +14,6 @@ class ItemController extends Controller
     public function index()
     {
         $items = Item::all();
-        dd($items);
         return view('items.index', compact('items'));
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ItemController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/items', [ItemController::class, 'index'])->name('items.index');
 Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
@@ -10,16 +10,3 @@ Route::get('/items/{id}', function () {})->name('items.show');
 Route::get('/items/{id}/edit', function () {})->name('items.edit');
 Route::put('/items/{id}', function () {})->name('items.update');
 Route::delete('/items/{id}', function () {})->name('items.destroy');
-
-// We voegen ook een redirect toe aan de routes die de hoofdpagina doorverwijst naar de '/items' route
-Route::redirect('/', '/items');
-
-// Route voor de indexpagina van items
-Route::get('/items', function () {
-    return view('items.index');
-})->name('items.index');
-
-// Route voor de creatiepagina van items
-Route::get('/items/create', function () {
-    return view('items.create');
-})->name('items.create');
