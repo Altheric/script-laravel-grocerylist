9d3b0d1a599889fb48ac7c3b1e8a3af46ee5be36
Altheric <mauricedx@gmail.com>
2024-08-23 11:32:40 +0200

    Added partials, controllers and basic DB retrieval.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ItemController;
 
-Route::get('/items', function () {})->name('items.index');
-Route::get('/items/create', function () {})->name('items.create');
+Route::get('/items', [ItemController::class, 'index'])->name('items.index');
+Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
 Route::post('/items', function () {})->name('items.store');
 Route::get('/items/{id}', function () {})->name('items.show');
 Route::get('/items/{id}/edit', function () {})->name('items.edit');
@@ -12,3 +13,13 @@ Route::delete('/items/{id}', function () {})->name('items.destroy');
 
 // We voegen ook een redirect toe aan de routes die de hoofdpagina doorverwijst naar de '/items' route
 Route::redirect('/', '/items');
+
+// Route voor de indexpagina van items
+Route::get('/items', function () {
+    return view('items.index');
+})->name('items.index');
+
+// Route voor de creatiepagina van items
+Route::get('/items/create', function () {
+    return view('items.create');
+})->name('items.create');
